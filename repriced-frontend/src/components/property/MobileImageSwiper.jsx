0d8d6a3db71c6366af177f47e7a0d2e6fb5795a0
jakemaloney <jakemal85@gmail.com>
2025-06-15 19:31:39 +1000

    Contiue fixing swiper: needs to be centred

--- repriced-frontend/src/components/property/MobileImageSwiper.jsx
+++ repriced-frontend/src/components/property/MobileImageSwiper.jsx
@@ -6,25 +6,31 @@ import "swiper/css/pagination";
 
 const MobileImageSwiper = ({ imageUrl, title }) => (
   <div className="block md:hidden h-[220px]">
-    <Swiper
-      modules={[Pagination]}
-      spaceBetween={0}
-      slidesPerView={1}
-      pagination={{ clickable: true }}
-      className="!rounded-none"
-    >
-      {Array(5)
-        .fill(null)
-        .map((_, i) => (
-          <SwiperSlide key={i}>
-            <img
-              src={imageUrl}
-              alt={`${i + 1} of ${title}`}
-              className="w-full h-[400px] object-cover"
-            />
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <div className="relative rounded-xl overflow-hidden border border-gray-200 shadow-sm">
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={0}
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+          el: ".swiper-pagination", // 👈 This tells Swiper where to render the bullets
+        }}
+        className="custom-swiper"
+      >
+        {Array(5)
+          .fill(null)
+          .map((_, i) => (
+            <SwiperSlide key={i}>
+              <img
+                src={imageUrl}
+                alt={`${i + 1} of ${title}`}
+                className="w-full h-[400px] object-cover"
+              />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+      <div className="absolute bottom-3 left-1/2 -translate-x-1/2 z-10 swiper-pagination bg-white/70 px-3 py-1 rounded-full shadow-sm border-red-600" />
+    </div>
   </div>
 );
 export default MobileImageSwiper;
